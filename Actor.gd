b8eccc034b9e4f0a738ba9c71747635b219dbd5b
Dundies Online <dundies.online@gmail.com>
2020-03-15 17:04:26 +0800

    Create jummping, animations jump run sommersault, reimport sprites to non-filter. Need to redo animation, should be tide to something (Finite state)

--- Actor.gd
+++ Actor.gd
@@ -4,6 +4,5 @@ class_name Actor
 const FLOOR_NORMAL: = Vector2.UP;
 export var gravity: = 1000.00;
 
-export var speed: = Vector2(300.0, 1000.0);
 
 var _velocity: = Vector2.ZERO;
--- actors/Player.gd
+++ actors/Player.gd
@@ -1,11 +1,50 @@
 extends Actor
 
+export var speed: = Vector2(250.0, 500.0);
+const running_speed = 300.00;
+
+var direction = 1;
+var jump_times = 0;
+
 func _physics_process(delta: float) -> void:
 
-	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0;
-	var direction = get_direction();
-	_velocity = calc_move_velocity(_velocity, direction, speed, is_jump_interrupted);
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL);
+#	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0;
+#	var direction = get_direction();
+#	_velocity = calc_move_velocity(_velocity, direction, speed, is_jump_interrupted);
+	var new_velocity = _velocity;
+
+	if is_on_floor():
+		get_node("AnimatedSprite").play("running")
+		jump_times = 0;
+
+	if is_on_wall():
+		direction *= -1;
+		flip_sprite()
+
+	#lol
+	if Input.is_action_just_pressed("jump") and jump_times < 3:
+		if jump_times == 0:
+			get_node("AnimatedSprite").play("jumping")
+			new_velocity.y = speed.y * -1;
+		elif jump_times == 1 :
+			get_node("AnimatedSprite").play("sommersault")
+		elif jump_times == 2 :
+			get_node("AnimatedSprite").play("jumping")
+			new_velocity.y = speed.y * -1; #more ?
+		jump_times += 1;
+#		elif jump_times == 2:
+#			jump_times = 0;
+	
+	new_velocity.y += gravity * delta;
+	new_velocity.x = direction * running_speed;
+	
+	_velocity = move_and_slide(new_velocity, FLOOR_NORMAL);
+
+func flip_sprite() -> void:
+	if direction > 0:
+		get_node("AnimatedSprite").flip_h = false;
+	else:
+		get_node("AnimatedSprite").flip_h = true;
 
 func get_direction() -> Vector2:
 	return Vector2(
